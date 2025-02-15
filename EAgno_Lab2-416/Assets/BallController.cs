b2fe959bba9930bcb9acc11b2d5795bf46f44404
EAgno <eagno@student.ubc.ca>
2025-02-15 12:26:11 -0800

    Finished BallController & InputManager

---
+++ EAgno_Lab2-416/Assets/BallController.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+[RequireComponent(typeof(Rigidbody))]
+public class BallController : MonoBehaviour
+{
+    [SerializeField] private float force = 1f;
+    [SerializeField] private Transform ballAnchor;
+    [SerializeField] private Transform launchIndicator;
+
+    private bool isBallLaunched = false;
+    private Rigidbody ballRB;
+    [SerializeField] private InputManager inputManager;
+
+    void Start()
+    {
+        ballRB = GetComponent<Rigidbody>();
+        Cursor.lockState = CursorLockMode.Locked;
+        inputManager.OnSpacePressed.AddListener(LaunchBall);
+        ResetBall();
+    }
+
+    void Update()
+    {
+
+    }
+
+    private void LaunchBall()
+    {
+        if (isBallLaunched) return;
+        isBallLaunched = true;
+        transform.parent = null;
+        ballRB.isKinematic = false;
+        ballRB.AddForce(launchIndicator.forward * force, ForceMode.Impulse);
+        launchIndicator.gameObject.SetActive(false);
+    }
+
+    public void ResetBall()
+    {
+        isBallLaunched = false;
+
+        ballRB.isKinematic = true;
+        launchIndicator.gameObject.SetActive(true);
+        transform.parent = ballAnchor;
+        transform.localPosition = Vector3.zero;
+    }
+}
